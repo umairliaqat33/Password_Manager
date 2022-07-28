767ccefacc38f1e322cef17becbf0ff09616bc70
umairliaqat33 <umairliaqat552@gmail.com>
2022-07-28 22:36:17 +0500

    made user able to select the length of password and select type of text he wants in password. Also set up the initial length of password to 8 characters. task1 id: r5IUKLr8 task2 id: olAXlLlP

--- lib/main.dart
+++ lib/main.dart
@@ -3,20 +3,10 @@ import 'dart:math';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:fluttertoast/fluttertoast.dart';
+import 'package:password_manager/screens/password_creating_screen.dart';
 
 void main() {
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      home: const MyHomePage(),
-    );
-  }
+  runApp(const MyHomePage());
 }
 
 class MyHomePage extends StatefulWidget {
@@ -27,73 +17,20 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-
-  late TextEditingController textController=TextEditingController();
-  String copiedText="";
   @override
-  String generateRandomString(int length) {
-    Random _random = Random();
-    const _availableChars =
-        'AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz1234567890';
-    final randomString = List.generate(length,
-            (index) => _availableChars[_random.nextInt(_availableChars.length)])
-        .join();
-
-    return randomString;
-  }
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: SafeArea(
-        child: Column(
-          children: [
-            TextField(
-              decoration: InputDecoration(
-                label: Text("Your Password"),
-                border: OutlineInputBorder(
-                  borderSide: BorderSide(color: Colors.red)
-                ),
-              ),
-              obscureText: false,
-              enabled: false,
-              controller: textController,
-            ),
-            Row(
-              children: [
-                TextButton(
-                  onPressed: () {
-                    generateRandomString(10);
-                    setState(() {
-                      textController=TextEditingController(text: generateRandomString(10));
-                    });
-                  },
-                  style: ButtonStyle(
-                    foregroundColor: MaterialStateColor.resolveWith(
-                        (states) => Colors.white),
-                    backgroundColor:
-                        MaterialStateColor.resolveWith((states) => Colors.blue),
-                  ),
-                  child: Text("Generate"),
-                ),
-                SizedBox(
-                  width: 20,
-                ),
-                TextButton(
-                  onPressed: () {
-                    Clipboard.setData(ClipboardData(text: textController.text));
-                    Fluttertoast.showToast(msg: "Text Copied");
-                  },
-                  style: ButtonStyle(
-                    foregroundColor: MaterialStateColor.resolveWith(
-                        (states) => Colors.white),
-                    backgroundColor:
-                        MaterialStateColor.resolveWith((states) => Colors.blue),
-                  ),
-                  child: Text("Copy"),
-                ),
-              ],
-            ),
-          ],
+    return MaterialApp(
+      theme: ThemeData(
+        appBarTheme: AppBarTheme(color: Colors.purple),
+        checkboxTheme: CheckboxThemeData(
+          fillColor:
+              MaterialStateColor.resolveWith((states) => Colors.purpleAccent),
+          checkColor: MaterialStateColor.resolveWith((states) => Colors.white),
         ),
+        primaryColor: Colors.purple,
+      ),
+      home: Scaffold(
+        body: PasswordCreation(),
       ),
     );
   }
---
+++ lib/screens/password_creating_screen.dart
@@ -0,0 +1,282 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+
+class PasswordCreation extends StatefulWidget {
+  const PasswordCreation({Key? key}) : super(key: key);
+
+  @override
+  State<PasswordCreation> createState() => _PasswordCreationState();
+}
+
+class _PasswordCreationState extends State<PasswordCreation> {
+  late TextEditingController textController = TextEditingController();
+  int length = 8;
+  bool isBoxErrorVisible = false;
+  bool isCopyErrorVisible = false;
+  String generatedString = "";
+  bool isCharacter = true;
+  bool isNumbers = true;
+  bool isSpecialCharacters = true;
+  static const String chars =
+      'AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz';
+  static const String numbers = '1234567890';
+  static const String specialCharacters = '!@#\\\$%^&*()_+/.,`';
+
+  void selectText() {
+    if (isCharacter == true &&
+        isSpecialCharacters == true &&
+        isNumbers == true) {
+      randomCharString(length, chars + specialCharacters + numbers);
+    } else {
+      if (isCharacter == true) {
+        if (isNumbers == true) {
+          randomCharString(length, chars + numbers);
+        } else if (isSpecialCharacters == true) {
+          randomCharString(length, chars + specialCharacters);
+        } else {
+          randomCharString(length, chars);
+        }
+      } else if (isNumbers == true) {
+        if (isCharacter == true) {
+          randomCharString(length, chars + numbers);
+        } else if (isSpecialCharacters == true) {
+          randomCharString(length, numbers + specialCharacters);
+        } else {
+          randomCharString(length, numbers);
+        }
+      } else if (isSpecialCharacters == true) {
+        if (isCharacter == true) {
+          randomCharString(length, chars + specialCharacters);
+        } else if (isNumbers == true) {
+          randomCharString(length, numbers + specialCharacters);
+        } else {
+          randomCharString(length, specialCharacters);
+        }
+      } else {
+        textController.clear();
+        setState(() {
+          isBoxErrorVisible = true;
+        });
+      }
+    }
+  }
+
+  void randomCharString(int length, String text) {
+    Random _random = Random();
+
+    final randomString =
+    List.generate(length, (index) => text[_random.nextInt(text.length)])
+        .join();
+    setState(() {
+      textController.text = randomString;
+    });
+  }
+  @override
+  void initState() {
+    selectText();
+    super.initState();
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Password Manager"),
+      ),
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 40.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              SizedBox(
+                width: 300,
+                child: TextField(
+                  decoration: InputDecoration(
+                    label: Text(
+                      "Your Password",
+                      style: TextStyle(color: Colors.purple),
+                    ),
+                    disabledBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(10)),
+                      borderSide: BorderSide(color: Colors.purple),
+                    ),
+                  ),
+                  // obscureText: false,
+                  enabled: false,
+                  controller: textController,
+                ),
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              Visibility(
+                visible: isCopyErrorVisible,
+                child: Text(
+                  "Please generate password first",
+                  style: TextStyle(
+                    color: Colors.red,
+                    fontSize: 15,
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              Row(
+                children: [
+                  TextButton(
+                    onPressed: () {
+                      setState(() {
+                        isBoxErrorVisible = false;
+                      });
+                      selectText();
+                    },
+                    style: ButtonStyle(
+                      foregroundColor: MaterialStateColor.resolveWith(
+                              (states) => Colors.white),
+                      backgroundColor: MaterialStateColor.resolveWith(
+                              (states) => Colors.purple),
+                    ),
+                    child: Text("Generate"),
+                  ),
+                  SizedBox(
+                    width: 20,
+                  ),
+                  TextButton(
+                    onPressed: () {
+                      if (!textController.text.isEmpty) {
+                        Clipboard.setData(
+                            ClipboardData(text: textController.text));
+                        Fluttertoast.showToast(msg: "Text Copied");
+                        setState(() {
+                          isCopyErrorVisible = false;
+                        });
+                      } else {
+                        setState(() {
+                          isCopyErrorVisible = true;
+                        });
+                      }
+                    },
+                    style: ButtonStyle(
+                      foregroundColor: MaterialStateColor.resolveWith(
+                              (states) => Colors.white),
+                      backgroundColor: MaterialStateColor.resolveWith(
+                              (states) => Colors.purple),
+                    ),
+                    child: Text("Copy"),
+                  ),
+                ],
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              Text(
+                "Select checkbox to generate password",
+                style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 15,
+                    color: Colors.purple),
+              ),
+              Visibility(
+                visible: isBoxErrorVisible,
+                child: Text(
+                  "Please make a selection of text in password",
+                  style: TextStyle(
+                    color: Colors.red,
+                    fontSize: 15,
+                  ),
+                ),
+              ),
+              Row(
+                children: [
+                  Checkbox(
+                      value: isCharacter,
+                      onChanged: (value) {
+                        setState(() {
+                          isCharacter = !isCharacter;
+                        });
+                      }),
+                  Text(
+                    "Alphabets",
+                    style: TextStyle(color: Colors.purpleAccent),
+                  ),
+                ],
+              ),
+              Row(
+                children: [
+                  Checkbox(
+                      value: isSpecialCharacters,
+                      onChanged: (value) {
+                        setState(() {
+                          isSpecialCharacters = !isSpecialCharacters;
+                        });
+                      }),
+                  Text("Special Characters",
+                      style: TextStyle(color: Colors.purpleAccent)),
+                ],
+              ),
+              Row(
+                children: [
+                  Checkbox(
+                      value: isNumbers,
+                      onChanged: (value) {
+                        setState(() {
+                          isNumbers = !isNumbers;
+                        });
+                      }),
+                  Text("Digits",
+                      style: TextStyle(color: Colors.purpleAccent)),
+                ],
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              Row(
+                children: [
+                  Text("Password Length: ",
+                      style: TextStyle(color: Colors.purpleAccent)),
+                  Text("$length",
+                      style: TextStyle(
+                          color: Colors.purpleAccent,
+                          fontSize: 15,
+                          fontWeight: FontWeight.bold)),
+                ],
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              SliderTheme(
+                data: SliderTheme.of(context).copyWith(
+                  thumbShape: RoundSliderThumbShape(enabledThumbRadius: 15),
+                  overlayShape: RoundSliderOverlayShape(overlayRadius: 30),
+                  activeTrackColor: Colors.purple,
+                  thumbColor: Colors.purpleAccent,
+                  // overlayColor: Color(0x29EB1555),
+                  inactiveTrackColor: Color(0xFF8D8E98),
+                  inactiveTickMarkColor: Color(0xFF8D8E98),
+                  activeTickMarkColor: Colors.pink,
+                ),
+                child: Slider(
+                  value: length.toDouble(),
+                  divisions: 50,
+                  max: 50.0,
+                  onChanged: (newValue) {
+                    setState(() {
+                      length = newValue.toInt();
+                    });
+                    // print(length);
+                  },
+                  label: length.round().toString(),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
